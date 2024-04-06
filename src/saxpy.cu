d149e3f38d8c1479e13ac97da0168f6859842146
Daniel Luo <daniel@cosyne.com>
2024-04-06 10:44:40 +0100

    refactor(saxpy): reorganised

--- src/saxpy.cu
+++ src/saxpy.cu
@@ -5,38 +5,48 @@
 __global__
 void saxpy(int n, float a, float *x, float *y)
 {
-  int i = blockIdx.x*blockDim.x + threadIdx.x; // Calculate global index for each thread
-  if (i < n) y[i] = a*x[i] + y[i]; // Perform SAXPY operation if within bounds
+  // Calculate global index for each thread
+  int i = blockIdx.x*blockDim.x + threadIdx.x;
+
+  // Perform SAXPY operation if within bounds
+  if (i < n) y[i] = a*x[i] + y[i];
 }
 
 int main(void)
 {
-  int N = 1<<20; // Number of elements in arrays, 1<<20 represents 2^20
-  float *x, *y, *d_x, *d_y; // Pointers for host (x, y) and device (d_x, d_y) arrays
+  // Set array dimension to 2^20
+  int N = 1 << 20;
+  size_t bytes = N * sizeof(float);
 
-  // Allocate memory on the host
-  x = (float*)malloc(N*sizeof(float)); 
-  y = (float*)malloc(N*sizeof(float));
+  // Allocate host memory
+  float *x, *y;
+  x = (float*)malloc(bytes); 
+  y = (float*)malloc(bytes);
 
-  // Allocate memory on the device
-  cudaMalloc(&d_x, N*sizeof(float)); 
-  cudaMalloc(&d_y, N*sizeof(float));
+  // Allocate device memory
+  float *d_x, *d_y;
+  cudaMalloc(&d_x, bytes); 
+  cudaMalloc(&d_y, bytes);
 
   // Initialize arrays on the host
   for (int i = 0; i < N; i++) {
-    x[i] = 1.0f; // Set all elements of x to 1.0f
-    y[i] = 2.0f; // Set all elements of y to 2.0f
-  }
+    x[i] = 1.0f;
+    y[i] = 2.0f;
+  } 
 
   // Copy arrays from host to device
-  cudaMemcpy(d_x, x, N*sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_x, x, bytes, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_y, y, bytes, cudaMemcpyHostToDevice);
+
+  // Set number of threads per block and number of blocks
+  int threadsPerBlock = 256;
+  int numBlocks = (N + threadsPerBlock - 1) / threadsPerBlock;
 
   // Invoke the SAXPY kernel
-  saxpy<<<(N+255)/256, 256>>>(N, 2.0f, d_x, d_y); // (N+255)/256 calculates the number of blocks needed
+  saxpy<<<numBlocks, threadsPerBlock>>>(N, 2.0f, d_x, d_y);
 
   // Copy the result back to the host
-  cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
+  cudaMemcpy(y, d_y, bytes, cudaMemcpyDeviceToHost);
 
   // Compare results to expected result
   float maxError = 0.0f;
