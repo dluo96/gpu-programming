4c32f5842a0a636dfbc53654991cf284b8238b9d
Daniel Luo <dan.luo1996@gmail.com>
2024-04-28 11:02:43 +0100

    feat: clean up v5

--- src/sum_reduction/sum_reduction_v5.cu
+++ src/sum_reduction/sum_reduction_v5.cu
@@ -6,9 +6,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <math.h>
-
-#define SIZE 128
-#define SHMEM_SIZE 128 * 4
+#include "sum_reduction.h"
 
 // This function is a callable from the GPU - it can be thought of as helper
 // function for the kernel. `volatile` is specified to prevent caching in 
@@ -29,7 +27,7 @@ __device__ void warpReduce(volatile int* sdata, int ltid) {
 
 __global__ void sum_reduction_v5(int *g_input, int *g_output, int numElements) {
         // Allocate shared memory
-        __shared__ int sdata[SHMEM_SIZE];
+        __shared__ int sdata[SHMEM_BYTES];
 
         unsigned int tid = blockIdx.x * (blockDim.x * 2) + threadIdx.x;
         unsigned int ltid = threadIdx.x;
@@ -63,74 +61,4 @@ __global__ void sum_reduction_v5(int *g_input, int *g_output, int numElements) {
         if (ltid == 0) {
                 g_output[blockIdx.x] = sdata[0];
         }
-}
-
-void init_vector(int *v, int n) {
-        for (int i = 0; i < n; i++) {
-                v[i] = 1;
-        }
-}
-
-int main() {
-        int N = 1 << 24;
-        size_t bytes = N * sizeof(int);
-
-        int *input, *result;
-        int *d_input, *d_result;
-
-        // Allocate CPU and GPU memory, populate input, and copy to device
-        input = (int*)malloc(bytes);
-        result = (int*)malloc(bytes);
-        cudaMalloc(&d_input, bytes);
-        cudaMalloc(&d_result, bytes);
-        init_vector(input, N);
-        cudaMemcpy(d_input, input, bytes, cudaMemcpyHostToDevice);
-
-        // Block size (#threads) and grid size (#blocks)
-        int blockSize = SIZE;
-        int gridSize = (N/2 + blockSize - 1) / blockSize; // Division by 2 is for v4-v5
-
-        // CUDA events for timing kernels
-        cudaEvent_t start, stop;
-        cudaEventCreate(&start);
-        cudaEventCreate(&stop);
-        float milliseconds = 0;
-
-        // Perform first kernel call
-        cudaEventRecord(start);
-        sum_reduction_v5<<<gridSize, blockSize>>>(d_input, d_result, N);
-
-        // Track how many partial results are left to be added and perform kernel 
-        // decomposition with recursion.
-        // Note: although CUDA kernel launches are asynchronous, all GPU-related tasks
-        // placed in one stream (the default behavior) are executed sequentially. 
-        // Hence there is no need for `cudaDeviceSynchronize` between kernel calls here.
-        unsigned int numRemain = gridSize;
-        while(numRemain > 1) {
-                gridSize = (numRemain/2 + blockSize - 1) / blockSize; // Division 2 is for v4-v5
-                sum_reduction_v5<<<gridSize, blockSize>>>(d_result, d_result, numRemain);
-                numRemain = gridSize;
-        }
-        cudaEventRecord(stop);
-        cudaEventSynchronize(stop);
-        cudaEventElapsedTime(&milliseconds, start, stop);
-
-        // Copy to host
-        cudaMemcpy(result, d_result, bytes, cudaMemcpyDeviceToHost);
-
-        // Check result
-        printf("Result: %d \n", result[0]);
-        assert(result[0] == N);
-        printf("Success! Computed sum reduction.\n");
-        printf("Time elapsed: %f milliseconds\n", milliseconds);
-
-        // Cleanup
-        cudaFree(d_input);
-        cudaFree(d_result);
-        free(input);
-        free(result);
-        cudaEventDestroy(start);
-        cudaEventDestroy(stop);
-
-        return 0;
 }
