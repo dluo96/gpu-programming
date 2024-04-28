fe6cafc290df967f92795cdfcadf38eefa44140b
Daniel Luo <dan.luo1996@gmail.com>
2024-04-28 11:00:08 +0100

    feat: update main function

--- src/sum_reduction/main.cu
+++ src/sum_reduction/main.cu
@@ -9,56 +9,65 @@ void init_vector(int *a, int N) {
 }
 
 int main() {
-    int N = 1 << 24;
-    size_t bytes = N * sizeof(int);
+        int N = 1 << 24;
+        size_t bytes = N * sizeof(int);
 
-    int *input, *result;
-    int *d_input, *d_result;
-    input = (int*)malloc(bytes);
-    result = (int*)malloc(bytes);
-    cudaMalloc(&d_input, bytes);
-    cudaMalloc(&d_result, bytes);
-    init_vector(input, N);
-    cudaMemcpy(d_input, input, bytes, cudaMemcpyHostToDevice);
+        int *input, *result;
+        int *d_input, *d_result;
 
-    // Threads, blocks, grids, and dynamic shared memory
-    int blockSize = SHMEM_LEN;
-    int gridSize = (N/2 + blockSize - 1) / blockSize; // Division by 2 is for v4-v5
-    // int shmemSize = blockSize * sizeof(int);
+        // Allocate CPU and GPU memory, populate input, and copy to device
+        input = (int*)malloc(bytes);
+        result = (int*)malloc(bytes);
+        cudaMalloc(&d_input, bytes);
+        cudaMalloc(&d_result, bytes);
+        init_vector(input, N);
+        cudaMemcpy(d_input, input, bytes, cudaMemcpyHostToDevice);
 
-    // CUDA events for timing kernels
-    cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-    float milliseconds = 0;
+        // Block size (#threads) and grid size (#blocks)
+        int blockSize = SHMEM_LEN;
+        int gridSize = (N/2 + blockSize - 1) / blockSize; // Division by 2 is for v4-v5
 
-    // Kernel decomposition with recursion
-    cudaEventRecord(start);
-    sum_reduction_v4<<<gridSize, blockSize>>>(d_input, d_result, N);
+        // CUDA events for timing kernels
+        cudaEvent_t start, stop;
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+        float milliseconds = 0;
 
-    unsigned int numRemain = gridSize;
-    while(numRemain > 1) {
-        gridSize = (numRemain/2 + blockSize - 1) / blockSize; // Division by 2 is for v4-v5
-        sum_reduction_v4<<<gridSize, blockSize>>>(d_result, d_result, numRemain);
-        numRemain = gridSize;
-    }
-    cudaEventRecord(stop);
-    cudaEventSynchronize(stop);
-    cudaEventElapsedTime(&milliseconds, start, stop);
+        // Perform first kernel call
+        cudaEventRecord(start);
+        sum_reduction_v5<<<gridSize, blockSize>>>(d_input, d_result, N);
 
-    cudaMemcpy(result, d_result, bytes, cudaMemcpyDeviceToHost);
-    assert(result[0] == N);
-    printf("Success! Computed sum reduction.\n");
-    printf("Result: %d\n", result[0]);
-    printf("Time elapsed: %f milliseconds\n", milliseconds);
+        // Track how many partial results are left to be added and perform kernel 
+        // decomposition with recursion.
+        // Note: although CUDA kernel launches are asynchronous, all GPU-related tasks
+        // placed in one stream (the default behavior) are executed sequentially. 
+        // Hence there is no need for `cudaDeviceSynchronize` between kernel calls here.
+        unsigned int numRemain = gridSize;
+        while(numRemain > 1) {
+                gridSize = (numRemain/2 + blockSize - 1) / blockSize; // Division 2 is for v4-v5
+                sum_reduction_v5<<<gridSize, blockSize>>>(d_result, d_result, numRemain);
+                numRemain = gridSize;
+        }
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&milliseconds, start, stop);
 
-    // Cleanup
-    cudaFree(d_input);
-    cudaFree(d_result);
-    free(input);
-    free(result);
-    cudaEventDestroy(start);
-    cudaEventDestroy(stop);
+        // Copy to host
+        cudaMemcpy(result, d_result, bytes, cudaMemcpyDeviceToHost);
 
-    return 0;
+        // Check result
+        printf("Result: %d \n", result[0]);
+        assert(result[0] == N);
+        printf("Success! Computed sum reduction.\n");
+        printf("Time elapsed: %f milliseconds\n", milliseconds);
+
+        // Cleanup
+        cudaFree(d_input);
+        cudaFree(d_result);
+        free(input);
+        free(result);
+        cudaEventDestroy(start);
+        cudaEventDestroy(stop);
+
+        return 0;
 }
