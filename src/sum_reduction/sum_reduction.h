71cdceec5fa1e3c2c86e2006016c44c31f65a96d
Daniel Luo <dan.luo1996@gmail.com>
2024-04-28 11:06:26 +0100

    feat: consolidate v5 with other versions

--- src/sum_reduction/sum_reduction.h
+++ src/sum_reduction/sum_reduction.h
@@ -12,5 +12,7 @@ __global__ void sum_reduction_v1(int *g_input, int *g_output, int numElements);
 __global__ void sum_reduction_v3(int *g_input, int *g_output, int numElements);
 __global__ void sum_reduction_v2(int *g_input, int *g_output, int numElements);
 __global__ void sum_reduction_v4(int *g_input, int *g_output, int numElements);
+__global__ void sum_reduction_v5(int *g_input, int *g_output, int numElements);
+__device__ void warpReduce(volatile int* sdata, int ltid);
 
 #endif
